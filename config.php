d86d0e157a7e75de868e2478fde89db60d6cbe16
Lgullestrup <lgullestrup@gmail.com>
2022-09-21 23:13:49 +1000

    added build steps

--- config.php
+++ config.php
@@ -3,25 +3,24 @@
 // Place all app configs here
 date_default_timezone_set('UTC');
 
-// Application name
-define('APP_NAME', 'Tripwire');
+define('APP_NAME', getenv('APP_NAME'));
 
 // Content file server (Use CDN here if you have one) - used for serving images, css, js files
-define('CDN_DOMAIN', 'localhost');
+define('CDN_DOMAIN', getenv('APP_DOMAIN'));
 
 // EVE SDE table name
 define('EVE_DUMP', 'eve_carnyx');
 
 // Enable Tripwire API?
-define('TRIPWIRE_API', false);
+define('TRIPWIRE_API', getenv('API_ENABLED'));
 
 // EVE API userAgent
 define('USER_AGENT', 'Tripwire Server - adminEmail@example.com');
 
 // EVE SSO info
-define('EVE_SSO_CLIENT', 'clientID');
-define('EVE_SSO_SECRET', 'secret');
-define('EVE_SSO_REDIRECT', 'http://localhost/index.php?mode=sso');
+define('EVE_SSO_CLIENT', getenv('EVE_SSO_CLIENT'));
+define('EVE_SSO_SECRET', getenv('EVE_SSO_SECRET'));
+define('EVE_SSO_REDIRECT', getenv('EVE_SSO_CALLBACK'));
 
 // Discord integration
 /*define('DISCORD_WEB_HOOK', array(
--- db.inc.php
+++ db.inc.php
@@ -2,13 +2,14 @@
 
 try {
     $mysql = new PDO(
-        'mysql:host=localhost;dbname=tripwire_database;charset=utf8',
-        'username',
-        'password',
+        'mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';dbname='.getenv('DB_NAME').';charset=utf8',
+        getenv('DB_USERNAME'),
+        getenv('DB_PASSWORD'),
         Array(
             PDO::ATTR_PERSISTENT     => true
         )
     );
+    
 } catch (PDOException $error) {
     error_log($error->getMessage());
 }
